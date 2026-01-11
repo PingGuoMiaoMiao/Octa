ab12915ec6a7fbec9181df93ea817cb017d216f6
苹果喵喵 <3226742838@qq.com>
2026-01-11 23:42:33 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is ahead of 'origin/main' by 1 commit.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	modified:   src/editor.rs
    	modified:   src/editor/terminal.rs
    	modified:   src/main.rs

--- src/editor.rs
+++ src/editor.rs
@@ -1,82 +1,130 @@
-#![warn(clippy::all, clippy::pedantic)]
-use crossterm::event::{read, Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
+use crossterm::event::{Event::{self, Key}, KeyCode::{self}, KeyEvent, KeyEventKind, KeyModifiers, read};
 use std::io::Error;
+use std::cmp::min;
 mod terminal;
-use terminal::{Position, Size, Terminal};
+use terminal::{Terminal, Size, Position};
+
 const NAME: &str = env!("CARGO_PKG_NAME");
 const VERSION: &str = env!("CARGO_PKG_VERSION");
-pub struct Editor {
+const EDITION: &str =
+    match option_env!("CARGO_PKG_EDITION") {
+        Some(v) => v,
+        None => "unknown",
+    };
+
+#[derive(Clone, Copy, Default)]
+pub struct Location {
+    pub x: usize,
+    pub y: usize
+}
+
+#[derive(Default)]
+pub struct Editor{
     should_quit: bool,
+    location: Location
 }
 
 impl Editor {
-    pub const fn default() -> Self {
-        Self { should_quit: false }
-    }
-    pub fn run(&mut self) {
+
+    pub fn run(&mut self){
         Terminal::initialize().unwrap();
         let result = self.repl();
         Terminal::terminate().unwrap();
         result.unwrap();
     }
 
-    fn repl(&mut self) -> Result<(), Error> {
-        loop {
-            self.refresh_screen()?;
-            if self.should_quit {
-                break;
+    fn move_point(&mut self, key_code: KeyCode) -> Result<(), Error> {
+        let Location { mut x, mut y } = self.location;
+        let Size { height, width } = Terminal::size()?;
+        match key_code {
+            KeyCode::Up => {
+                y = y.saturating_sub(1);
             }
-            let event = read()?;
-            self.evaluate_event(&event);
+            KeyCode::Down => {
+                y = min(height.saturating_sub(1), y.saturating_add(1));
+            }
+            KeyCode::Left => {
+                x = x.saturating_sub(1);
+            }
+            KeyCode::Right => {
+                x = min(width.saturating_sub(1), x.saturating_add(1));
+            }
+            KeyCode::PageUp => {
+                y = 0;
+            }
+            KeyCode::PageDown => {
+                y = height.saturating_sub(1);
+            }
+            KeyCode::Home => {
+                x = 0;
+            }
+            KeyCode::End => {
+                x = width.saturating_sub(1);
+            }
+            _ => (),
         }
+        self.location = Location { x, y };
         Ok(())
     }
 
-    fn evaluate_event(&mut self, event: &Event) {
-        if let Key(KeyEvent {
-            code, modifiers, ..
-        }) = event
-        {
-            match code {
-                Char('q') if *modifiers == KeyModifiers::CONTROL => {
-                    self.should_quit = true;
+    fn evaluate_event(&mut self,event: &Event) -> Result<(), Error> {
+            if let Key(KeyEvent{
+            code, modifiers, kind: KeyEventKind::Press,..
+        }) = event {
+                match code {
+                    KeyCode::Char('q') if *modifiers == KeyModifiers::CONTROL => {
+                        self.should_quit = true;
+                    }
+                    KeyCode::Up
+                    | KeyCode::Down
+                    | KeyCode::Left
+                    | KeyCode::Right
+                    | KeyCode::PageDown
+                    | KeyCode::PageUp
+                    | KeyCode::End
+                    | KeyCode::Home => {
+                        self.move_point(*code)?;
+                    }
+                    _ => (), 
                 }
-                _ => (),
             }
-        }
+            Ok(())
     }
 
-    fn refresh_screen(&self) -> Result<(), Error> {
-        Terminal::hide_cursor()?;
+   fn refresh_screen(&self) -> Result<(), Error> {
+        Terminal::hide_caret()?;
+        Terminal::move_caret_to(Position::default())?;
         if self.should_quit {
             Terminal::clear_screen()?;
             Terminal::print("Goodbye.\r\n")?;
         } else {
             Self::draw_rows()?;
-            Terminal::move_cursor_to(Position { x: 0, y: 0 })?;
+            Terminal::move_caret_to(Position {
+                col: self.location.x,
+                row: self.location.y,
+            })?;
         }
-        Terminal::show_cursor()?;
+        Terminal::show_caret()?;
         Terminal::execute()?;
         Ok(())
     }
+
     fn draw_welcome_message() -> Result<(), Error> {
-        let mut welcome_message = format!("{NAME} editor -- version {VERSION}");
+        let mut welcome_message = format!("{NAME} editor -- version {VERSION} editor {EDITION}");
         let width = Terminal::size()?.width;
         let len = welcome_message.len();
         // we allow this since we don't care if our welcome message is put _exactly_ in the middle.
         // it's allowed to be a bit to the left or right.
         #[allow(clippy::integer_division)]
         let padding = (width.saturating_sub(len)) / 2;
+
         let spaces = " ".repeat(padding.saturating_sub(1));
         welcome_message = format!("~{spaces}{welcome_message}");
         welcome_message.truncate(width);
-        Terminal::print(welcome_message)?;
+        Terminal::print(&welcome_message)?;
         Ok(())
     }
-    fn draw_empty_row() -> Result<(), Error> {
-        Terminal::print("~")?;
-        Ok(())
-    }
+
     fn draw_rows() -> Result<(), Error> {
         let Size { height, .. } = Terminal::size()?;
         for current_row in 0..height {
@@ -92,6 +140,23 @@ impl Editor {
             if current_row.saturating_add(1) < height {
                 Terminal::print("\r\n")?;
             }
+        }
+        Ok(())
+    }
+
+    fn draw_empty_row() -> Result<(), Error> {
+        Terminal::print("~")?;
+        Ok(())
+    }
+
+    fn repl(&mut self) -> Result<(), Error>{
+        loop {
+           self.refresh_screen()?;
+            if self.should_quit {
+                break;
+            }
+            let event = read()?;
+            self.evaluate_event(&event)?;
         }
         Ok(())
     }
--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -1,21 +1,28 @@
 use crossterm::cursor::{Hide, MoveTo, Show};
-use crossterm::{queue, Command};
+use crossterm::{Command, queue};
 use crossterm::style::Print;
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode, size, Clear, ClearType};
 use std::io::{stdout, Error, Write};
-use core::fmt::Display;
+/// Represents the Terminal.
+/// Edge Case for platforms where `usize` < `u16`:
+/// Regardless of the actual size of the Terminal, this representation
+/// only spans over at most `usize::MAX` or `u16::size` rows/columns, whichever is smaller.
+/// Each size returned truncates to min(`usize::MAX`, `u16::MAX`)
+/// And should you attempt to set the caret out of these bounds, it will also be truncated.
+pub struct Terminal {}
 
-#[derive(Copy, Clone)]
-pub struct Size {
+#[derive(Clone, Copy)]
+pub struct Size{
     pub height: usize,
     pub width: usize,
 }
-#[derive(Copy, Clone)]
+
+
+#[derive(Clone, Copy, Default)]
 pub struct Position {
-    pub x: usize,
-    pub y: usize,
+    pub col : usize,
+    pub row: usize
 }
-pub struct Terminal;
 
 impl Terminal {
     pub fn terminate() -> Result<(), Error> {
@@ -26,7 +33,6 @@ impl Terminal {
     pub fn initialize() -> Result<(), Error> {
         enable_raw_mode()?;
         Self::clear_screen()?;
-        Self::move_cursor_to(Position { x: 0, y: 0 })?;
         Self::execute()?;
         Ok(())
     }
@@ -38,31 +44,32 @@ impl Terminal {
         Self::queue_command(Clear(ClearType::CurrentLine))?;
         Ok(())
     }
-    /// Moves the cursor to the given Position.
-    /// # Arguments
-    /// * `Position` - the  `Position` to move the cursor to. Will be truncated to `u16::MAX` if bigger.
-    pub fn move_cursor_to(position: Position) -> Result<(), Error> {
-        // clippy::as_conversions: See doc above
-        #[allow(clippy::as_conversions, clippy::cast_possible_truncation)]
-        Self::queue_command(MoveTo(position.x as u16, position.y as u16))?;
+    pub fn hide_caret() -> Result<(), Error> {
+        queue!(stdout(), Hide)?;
         Ok(())
     }
-    pub fn hide_cursor() -> Result<(), Error> {
-        Self::queue_command(Hide)?;
-        Ok(())
-    }
-    pub fn show_cursor() -> Result<(), Error> {
+    pub fn show_caret() -> Result<(), Error> {
         Self::queue_command(Show)?;
         Ok(())
     }
-    pub fn print<T: Display>(string: T) -> Result<(), Error> {
+    /// Moves the cursor to the given Position.
+    /// # Arguments
+    /// * `Position` - the  `Position`to move the cursor to. Will be truncated to `u16::MAX` if bigger.
+    pub fn move_caret_to(position: Position) -> Result<(), Error> {
+      // clippy::as_conversions: See doc above
+        #[allow(clippy::as_conversions, clippy::cast_possible_truncation)]
+         Self::queue_command(MoveTo(position.col as u16, position.row as u16))?;
+        Ok(())
+    }
+    pub fn print(string: &str) -> Result<(), Error> {
         Self::queue_command(Print(string))?;
         Ok(())
     }
     /// Returns the current size of this Terminal.
-    /// Edge Case for systems with `usize` < `u16`: any coordinate will be represented as `usize`.
+    /// Edge Case for systems with `usize` < `u16`:
+    /// * A `Size` representing the terminal size. Any coordinate `z` truncated to `usize` if `usize` < `z` < `u16`
     pub fn size() -> Result<Size, Error> {
-        let (width_u16, height_u16) = size()?;
+                let (width_u16, height_u16) = size()?;
         // clippy::as_conversions: See doc above
         #[allow(clippy::as_conversions)]
         let height = height_u16 as usize;
